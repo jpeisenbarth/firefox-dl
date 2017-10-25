3cc6a89b6fec9df4dc1e5e431330a2095b22932f
Jean-Philippe Eisenbarth <jpeisenbarth@users.noreply.github.com>
2017-10-25 11:33:37 +0200

    [update] Developer/Aurora part

--- firefox_dl_n_install.sh
+++ firefox_dl_n_install.sh
@@ -3,7 +3,7 @@
 echo "Choose the Firefox release you wanna install/upgrade [1|2|3|4]:"
 echo "1. Firefox Stable"
 echo "2. Firefox Beta"
-echo "3. Firefox Aurora"
+echo "3. Firefox Developer Edition"
 echo "4. Firefox Nightly"
 read -rp "> " CHOICE
 
@@ -22,8 +22,8 @@ elif [ $CHOICE = 2 ]; then
 	RELEASE="-beta"
 	URL=$(wget -q -O - https://www.mozilla.org/en-US/firefox/beta/all/ | egrep -o "href=.*?os=linux${ARCHI_SHORT}&amp;lang=en-US['"'"'"]" | sed -e 's/\&amp;/\&/g' -e 's/^href=//' -e 's/["'"'"']//g' )
 elif [ $CHOICE = 3 ]; then
-	RELEASE="-aurora"
-	URL=$(wget -q -O - https://www.mozilla.org/en-US/firefox/developer/all/ | egrep -o "href=['"'"'"].*?download.mozilla.org.*?firefox-aurora-latest-ssl.*?linux${ARCHI_SHORT}.*?en-US['"'"'"]" | sed -e 's/\&amp;/\&/g' -e 's/^href=//' -e 's/["'"'"']//g')
+	RELEASE="-developer"
+	URL=$(wget -q -O - https://www.mozilla.org/en-US/firefox/developer/all/ | egrep -o "href=.*?os=linux${ARCHI_SHORT}&amp;lang=fr['"'"'"]" | sed -e 's/\&amp;/\&/g' -e 's/^href=//' -e 's/["'"'"']//g' )
 elif [ $CHOICE = 4 ]; then
 	URL=$(wget -q -O - http://nightly.mozilla.org | egrep -o "href=.*?firefox.*?linux-${ARCHI}\.tar\.bz2['"'"'"]" | sed -e 's/^href=//' -e 's/["'"'"']//g')
 	RELEASE="-nightly"
